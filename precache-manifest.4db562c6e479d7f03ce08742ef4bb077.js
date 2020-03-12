54107dec3eb8b06003d6e799670de1b161401b04
Julio Moros <jjmorosr@gmail.com>
2020-03-12 12:18:21 -0400

    Updates

---
+++ precache-manifest.4db562c6e479d7f03ce08742ef4bb077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f58f53729b3a6d504934",
+    "url": "/PaginaTest/static/css/main.a3681309.chunk.css"
+  },
+  {
+    "revision": "f58f53729b3a6d504934",
+    "url": "/PaginaTest/static/js/main.f58f5372.chunk.js"
+  },
+  {
+    "revision": "7968856bbd14ae87fc31",
+    "url": "/PaginaTest/static/css/1.b6dae422.chunk.css"
+  },
+  {
+    "revision": "7968856bbd14ae87fc31",
+    "url": "/PaginaTest/static/js/1.7968856b.chunk.js"
+  },
+  {
+    "revision": "e5bc284854b87e59dd18",
+    "url": "/PaginaTest/static/js/runtime~main.e5bc2848.js"
+  },
+  {
+    "revision": "0fad920e7e15f4852a1b3844022e6fd1",
+    "url": "/PaginaTest/index.html"
+  }
+];
